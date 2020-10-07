e9d1fb6b27262f29d6c22a1b72c56ce9e0366767
KevinCRB <70531317+KevinCRB@users.noreply.github.com>
2020-10-06 20:32:10 -0500

    Update Interfaz.java

--- Interfaz.java
+++ Interfaz.java
@@ -1,14 +1,9 @@
 package IU;
-import Logic.Main;
 import java.util.Scanner;
-
-public class Interfaz {
-    
+import Logic.Main;
+public class Interfaz { 
     public static void iniciar(){
         System.out.println("\tBienvenido a la tienda de personajes de Disney");
-    }
-    public static void iniciarPixar(){
-        System.out.println("\tBienvenido a la sección de Pixar");
     }
     public static void preguntarSeccion(){
         System.out.println("\nEscoja la opción que desea ver:\n1. Personajes de Pixar\n2. Personajes de Marvel\n3. Personajes de Star wars\n4. Salir de la tienda");
@@ -18,17 +13,18 @@ public class Interfaz {
 	int opcion = x.nextInt();
 	return opcion;
     }
-    public static void preguntarPersonajePixar(){
-        System.out.println("\nEscoja que tipo de personaje de Pixar desea ver:\n1. Protagonistas de Pixar\n2. Antagonistas de Pixar\n3. Volver a la sección anterior\n4. Salir de la tienda");
+    public static void preguntarPersonaje(String clase){
+        System.out.printf("\nEscoja que tipo de personaje de %s desea ver:\n1. Protagonistas de %s\n2. Antagonistas de %s\n3. Volver a la sección anterior\n4. Salir de la tienda\n",clase,clase,clase);
     }
-    public static void preguntarProtaPixar(){
-        System.out.println("\nEscoja que protagonista de Pixar desea ver:\n1. Nemo\n2. Mike Wazowski\n3. Buzz Lightyear\n4. Volver a la sección anterior\n5. Salir de la tienda");
+    public static void preguntarProta(String clase,String protagonistas[]){
+        System.out.printf("\nEscoja que protagonista de %s desea ver:\n1. %s\n2. %s\n3. %s\n4. Volver a la sección anterior\n5. Salir de la tienda\n",clase,protagonistas[0],protagonistas[1],protagonistas[2]);
     }
-    public static void preguntarAntPixar(){
-        System.out.println("\nEscoja que antagonista de Pixar desea ver:\n1. Darla\n2. Randall\n3. Sid\n4. Volver a la sección anterior\n5. Salir de la tienda");
+    public static void preguntarAnt(String clase,String antagonistas[]){
+        System.out.printf("\nEscoja que antagonista de %s desea ver:\n1. %s\n2. %s\n3. %s\n4. Volver a la sección anterior\n5. Salir de la tienda\n",clase,antagonistas[0],antagonistas[1],antagonistas[2]);
     }
     public static void preguntar(){
         System.out.println("\nEscoja una opción:\n1. Volver a la sección anterior\n2. Salir de la tienda");
     }
     
 }
+
